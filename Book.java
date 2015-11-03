a9b2a327699717b4d0a4d726c074927791d70181
mgob <thomas.sikma@gmail.com>
2015-11-03 18:25:54 -0500

    Book scaffolding

---
+++ Book.java
@@ -0,0 +1,9 @@
+public class Book extends Readable {
+	@Override
+	public int getPrice(){ // override to get the item price and add 2% (Environment Tax)
+
+	}
+	public int getListInfo(){ //Based on the value of Type(Book or eBook) print the list of Items
+		
+	}
+}
