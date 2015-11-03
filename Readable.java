765712adf0269390dbeed1ea028c77c6d59e7887
mgob <thomas.sikma@gmail.com>
2015-11-03 16:51:19 -0500

    Readable scaffolding

---
+++ Readable.java
@@ -0,0 +1,9 @@
+public class Readable extends Item {
+
+	protected String authorName;
+	public String getInfo(){} //Returns sNo, Name, Author name, etc in a string
+	@Override
+	public int getPrice(){ //override
+
+	}
+}
