5b03c8bfb2943d6bb9958cda5ef5834ec5d4c064
mgob <thomas.sikma@gmail.com>
2015-11-03 18:27:36 -0500

    Audio scaffolding

---
+++ Audio.java
@@ -0,0 +1,13 @@
+public class Audio extends Item {
+	protected String artistName;
+	public String getInfo(){ //Returns sNo, Name, Artist name, etc in a string
+
+	}
+	@Override
+	public int getPrice(){ //override
+
+	}
+	public int getListInfo(){ //Based on the value of Type(CD or MP3) print the list of Items
+
+	}
+}
