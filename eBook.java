f54c1ed06df6de1323d7cd12a7cf2e91187723b0
mgob <thomas.sikma@gmail.com>
2015-11-03 18:26:51 -0500

    eBook scaffolding

---
+++ eBook.java
@@ -0,0 +1,9 @@
+public class eBook extends Readable {
+	@Override
+	public int getPrice(){ // override and only call the parent's constructor to get base price.
+
+	}
+	public int getListInfo(){ //Based on the value of Type(Book or eBook) print the list of Items
+		
+	}
+}
