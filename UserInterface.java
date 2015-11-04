1fd3f4b27118e0dc551c716ba8cfbb5b9b26e021
mgob <thomas.sikma@gmail.com>
2015-11-04 16:54:59 -0500

    UserInterface scaffolding

---
+++ UserInterface.java
@@ -0,0 +1,12 @@
+public class UserInterface{
+	private int currentPage; 
+	//^ the page number (P1..P10)
+	public int getCurrentPage(){ // This method is for page navigation. Based on the values of the state variable, call different pages
+
+
+	}
+	public int changeCurrentPage(){ // This method is for page navigation. It should change to current page and show the content
+		
+
+	}
+}
