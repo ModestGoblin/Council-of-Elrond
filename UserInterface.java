c0666c80d5bde360a01e63639cc3b7593fd67668
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-18 11:51:50 -0500

    No longer self contained Methods now work through the main function

--- UserInterface.java
+++ UserInterface.java
@@ -1,37 +1,103 @@
+import java.util.Scanner;
+import java.io.*;
 public class UserInterface{
-	private int currentPage; 
+	private int currentPage;
+	private User loggedin;
+	private ShoppingCart usercart;
 	//^ the page number (P1..P10)
 	public int getCurrentPage(int toPage){
-		Scanner option = new Scanner(System.in);
 		if(toPage == 1){
+			Scanner input = new Scanner(System.in);
 			int choice = 0;
 			while(choice != 1 && choice != 2){
 				choice = 0;
 				System.out.println("1.Sign in");
 				System.out.println("2.Sign up");
-				choice = Integer.parseInt(option.next());
+				try{
+				choice = Integer.parseInt(input.next());
+				}
+				catch(NumberFormatException e){
+					System.out.println("Invalid Input");
+				}
 			}
 			if(choice == 1){
 				System.out.println("Please enter your username");
-				changeCurrentPage(3);
+				String name = input.next();
+				try{
+				BufferedReader names = new BufferedReader(new InputStreamReader(new FileInputStream("Users.txt")));
+				String check = null;
+				check = names.readLine();
+				System.out.println(check);
+				boolean next = true;
+				while(next == true){
+				if(name.equals(check)){
+					names.close();
+					loggedin = new User(name);
+					usercart = new ShoppingCart(name);
+					return(3);
+					}
+				else if(check == null){
+					next = false;
+					names.close();
+					return(4);
+				}
+				check = (names.readLine());
+				}
+				}
+				catch(FileNotFoundException e){
+					System.out.println("Users.txt file not found");
+				}
+				catch(IOException e){
+					
+				}
 			}
 			else if(choice == 2){
-				changeCurrentPage(2);
+				return(2);
 			}
 		}
 		else if (toPage == 2){
-			System.out.println("Now on P2");
-			changeCurrentPage(5);
+			Scanner input = new Scanner(System.in);
+			System.out.println("Please Choose your username");
+			String name = input.next();
+			try{
+			BufferedReader names = new BufferedReader(new InputStreamReader(new FileInputStream("Users.txt")));
+			String check = null;
+			check = names.readLine();
+			boolean next = true;
+			while(next == true){
+			if(name.equals(check)){
+				names.close();
+				System.out.println("Username already in use. Returning to Sign in/Sign Up selection Page");
+				}
+			else if(check == null){
+				next = false;
+				names.close();
+				BufferedWriter writer = new BufferedWriter(new FileWriter("Users.txt",true));
+				writer.newLine();
+				writer.append(name);
+				writer.close();
+			}
+			check = (names.readLine());
+			}
+			return(1);
+			}
+			catch(FileNotFoundException e){
+				System.out.println("Users.txt file not found");
+			}
+			catch(IOException e){
+				
+			}
 		}
 		else if(toPage == 3){
-			System.out.println("Now on P3");
-			changeCurrentPage(5);
+			System.out.println("Welcome " + loggedin.getUsername());
+			return(5);
 		}
 		else if(toPage == 4){
-			System.out.println("Now on P4");
-			changeCurrentPage(1);
+			System.out.println("Access Denied");
+			return(1);
 		}
 		else if(toPage == 5){
+			Scanner input = new Scanner(System.in);
 			System.out.println("Main Menu");
 			int choice = 0;
 			while(choice != 1 && choice != 2 && choice != 3){
@@ -39,34 +105,47 @@ public class UserInterface{
 				System.out.println("1.Browse Store By Category");
 				System.out.println("2.View Cart");
 				System.out.println("3.Return to Sign In");
-				choice = Integer.parseInt(option.next());
+				try{
+					choice = Integer.parseInt(input.next());
+				}
+				catch(NumberFormatException e){
+					System.out.println("Invalid Input");
+				}
 			}
+			input.close();
 			if(choice == 1){
-				changeCurrentPage(6);
+				return(6);
 			}
 			else if(choice == 2){
-				changeCurrentPage(7);
+				return(7);
 			}
 			else if(choice == 3){
-				changeCurrentPage(1);
+				return(1);
 			}
 		}
 		else if(toPage == 6){
+			Scanner input = new Scanner(System.in);
 			int choice = 0;
 			while(choice != -1 && choice != 1 && choice != 2){
 				System.out.println("1.Readables");
 				System.out.println("2.Audio");
 				System.out.println("Please enter your selection or enter -1 to return to the last menu");
-				choice = Integer.parseInt(option.next());
+				try{
+					choice = Integer.parseInt(input.next());
+				}
+				catch(NumberFormatException e){
+					System.out.println("Invalid Input");
+				}
 			}
+			input.close();
 			if(choice == 1){
-				changeCurrentPage(8);
+				return(8);
 			}
 			else if(choice == 2){
-				changeCurrentPage(9);
+				return(9);
 			}
 			else if(choice == -1){
-				changeCurrentPage(5);
+				return(5);
 			}
 		}
 		else if(toPage == 7){
@@ -89,8 +168,7 @@ public class UserInterface{
 	}
 	public int changeCurrentPage(int selection){
 		this.currentPage = selection;
-		this.getCurrentPage(selection);
-		return 0;
+		return currentPage;
 	}
 
 }
