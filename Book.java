324acd5a15dccfb42262b81f195ab4cda3b09956
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-20 10:24:27 -0500

    Commented

--- Book.java
+++ Book.java
@@ -1,12 +1,12 @@
-public class Book extends Readable {
-    public Book(String s) {
-        super(s);
-    }
+public class Book extends Readable {//class that contains information on Book items
+    public Book(String s) {//constructor
+        super(s);//call parent
+    }//end constructor
 	@Override
 	public int getPrice(){ // override to get the item price and add 2% (Environment Tax)
-        return (int)(price + price * 0.02);
-	}
+        return (int)(price + price * 0.02);//return price with tax
+	}//end getPrice
 	public String getListInfo(){ //Based on the value of Type(Book or eBook) print the list of Items
-        return super.getListInfo() + "\tBook";
-	}
-}
+        return super.getListInfo() + "\tBook";//return info on Book
+	}//end getListInfo
+}//end Book Class
