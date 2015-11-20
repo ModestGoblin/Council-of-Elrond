b433251707de868c6675d83e3059765dcf1508cf
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-20 11:35:59 -0500

    commented

--- CD.java
+++ CD.java
@@ -1,12 +1,12 @@
-public class CD extends Audio {
-    public CD(String s) {
-        super(s);
-    }
+public class CD extends Audio {//class containing info for CD items
+    public CD(String s) {//constructor
+        super(s);//call parent constructor
+    }//end of constructor
 	@Override
 	public int getPrice(){ // override to get the item price and add 2% (Environment Tax)
-        return (int)((double)price + ((double)price * 0.02));
-	}
-    public String getListInfo() {
-        return super.getListInfo() + "\tCD";
-    }
-}
+        return (int)((double)price + ((double)price * 0.02));//return newly calculated price
+	}//end of get price
+    public String getListInfo() {//function to return info about the item
+        return super.getListInfo() + "\tCD";//return information
+    }//end getListInfo
+}//end of class
