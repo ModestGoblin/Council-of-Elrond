9ca8d210ec37e0bf4f2f75541de3265bf03ddfaa
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-20 08:43:20 -0500

    Commented

--- HWK4_MACID.java
+++ HWK4_MACID.java
@@ -1,10 +1,9 @@
 //(contains the main method)
-public class HWK4_MACID {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        while (true) {
-        	ui.changeCurrentPage(ui.getCurrentPage());
-         
-    	}
-    }
-}
+public class HWK4_MACID {//class containing main method
+    public static void main(String[] args) {//main method
+        UserInterface ui = new UserInterface();//create a new user interface for page naviagtion and display
+        while (true) {//continue repeating forever
+        	ui.changeCurrentPage(ui.getCurrentPage());//change the page, based on inputs and outcomes of user action on currently displayed page
+    	}//end while loop
+    }//end of main
+}//emd of class HWK4
