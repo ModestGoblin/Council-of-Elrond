bc600a0741175ff503e4cd2af1755e27cd019dbf
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-23 16:07:56 -0500

    Complete!

--- HWK4_MACID.java
+++ HWK4_MACID.java
@@ -1,3 +1,9 @@
+/*
+* Name: Thomas Sikma Graham Van Deventer Nezar Dimitri
+* MacID: sikmat vandevgm dimitn
+* Student Number: 1428584 1404877 1422586
+* Description: The main class. This file will be used by the user, and has access to all other classes, calling them when needed
+*/
 //(contains the main method)
 public class HWK4_MACID {//class containing main method
     public static void main(String[] args) {//main method
