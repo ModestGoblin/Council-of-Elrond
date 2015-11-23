cb8c30fef09a4045d41bb1dfe632b46f9cfaa2f8
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-23 16:28:01 -0500

    Complete

--- User.java
+++ User.java
@@ -1,3 +1,9 @@
+/*
+* Name: Thomas Sikma Graham Van Deventer Nezar Dimitri
+* MacID: sikmat vandevgm dimitn
+* Student Number: 1428584 1404877 1422586
+* Description: The User class contains relevant information on Users, specifically the username. This username will be accessed when Shopping Cart fucnctions are used. Each instance of User will most likely have a Shopping Cart assoiciated with it
+*/
 public class User{//class that contains all information on a user
 	private String username;//the username of the user
     public User(String username) {//constructor
