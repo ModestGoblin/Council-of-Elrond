44a22c0105e407a50f1a23c9992bafa467a05927
Graham Van Deventer <gvandeventer124@gmail.com>
2015-11-23 10:37:52 -0500

    Comments

--- User.java
+++ User.java
@@ -1,9 +1,9 @@
-public class User{
-	private String username;
-    public User(String username) {
-        this.username = username;
-    }
+public class User{//class that contains all information on a user
+	private String username;//the username of the user
+    public User(String username) {//constructor
+        this.username = username;//assign User username to User object
+    }//end constructor
 	public String getUsername(){ // stores the username.
-        return username;
-	}
-}
+        return username;//grab and return to username
+	}//end getUsername
+}//end User class
